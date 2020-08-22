f33e2a4526d0885ef4b8d7129843b34708668a85
amateurbeekeeper <jamesianrowles@gmail.com>
2020-08-23 10:49:42 +1200

    Add get items V2 - getItemsV2

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 const data = {
-    getItemsV1 : function(){
+    getItemsV1: function () {
         return [
             {
-              itemName: 'butter',
-              brand: 'Tararua',
-              price: 3.99
+                itemName: 'butter',
+                brand: 'Tararua',
+                price: 3.99
             },
             {
                 itemName: 'butter',
@@ -20,22 +20,43 @@ const data = {
                 itemName: 'butter',
                 brand: 'SuperBudget',
                 price: 1.99
-              }
-          ];
+            }
+        ];
+    },
+
+    getItemsV2: async function () {
+        const response = await fetch("https://www.ishopnewworld.co.nz/Search?q=butter");
+        if (response.ok) {
+            const responseText = await response.text();
+            const parser = new DOMParser();
+
+            // html doc from parsed text from new world
+            const doc = parser.parseFromString(responseText, 'text/html');
+
+            // html array to js array
+            const itemArray = Array.from(doc.getElementsByClassName('js-product-card-footer'));
+
+            //
+           // console.log(itemArray);
+
+            // convert all elements to js objects
+            const jsArray = itemArray.map(element => JSON.parse(element.getAttribute('data-options')));
+
+            //
+            //console.log(jsArray);
+
+            // format converted data into expected json array
+            const formattedArray = jsArray.map(element => {
+                return {
+                    itemName: 'butter',
+                    brand: element.productName,
+                    price: element.ProductDetails.PricePerItem
+                }
+            }
+            );
+
+             //
+            //console.log(formattedArray);
+        }
     }
-} ;
-
-(async function (){  
-    const response = await fetch("https://www.ishopnewworld.co.nz/Search?q=butter");
-    if(response.ok){
-        const responseText = await response.text(); 
-       const parser = new DOMParser(); 
-       const doc = parser.parseFromString(responseText, 'text/html');
-    }
-})();
-
-
-const itemArray = Array.from(itemDoc.getElementsByClassName('js-product-card-footer'));
-console.log(itemArray);
-
-Array.from(document.getElementsByClassName('js-product-card-footer')).map(element => JSON.parse(element.getAttribute('data-options')));
+};
